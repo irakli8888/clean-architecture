93a6d5f501ab711fe56826422f1d44c34ef977a0
irakli kvachahia <iraklion93@mail.ru>
2023-04-07 11:31:02 +0400

    solid # fix EmployeeFacade name in App.java

--- architecture/src/main/java/solid/srp/impl/App.java
+++ architecture/src/main/java/solid/srp/impl/App.java
@@ -3,8 +3,8 @@ package solid.srp.impl;
 public class App {
     public static void main(String[] args) {
 
-        EmployeeFacade empolyeeFacade = new EmployeeFacade();
-        empolyeeFacade.cleanFood();
-        empolyeeFacade.cook();
+        EmployeeFacade employeeFacade = new EmployeeFacade();
+        employeeFacade.cleanFood();
+        employeeFacade.cook();
     }
 }
